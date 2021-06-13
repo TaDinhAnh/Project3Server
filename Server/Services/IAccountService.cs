eee9cb7005f033db667341a28b7fae04eb21b482
Nam <anh.td286@aptechlearning.edu.vn>
2021-06-13 21:24:10 +0700

    CRUD Account

---
+++ Server/Services/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Server.Models;
+namespace Server.Services
+{
+   public interface IAccountService
+    {
+        List<Account> FindAll();
+        Account Find(int idAcc);
+        string Create(Account acc);
+        string Del(int idAcc);
+        string Update(Account acc);
+    }
+}
